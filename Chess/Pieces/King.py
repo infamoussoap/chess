d83af8c16346e809b037ef4b159a18170d999a8c
infamoussoap <james.chok1000+github@gmail.com>
2022-07-24 09:26:36 +1000

    valid move added for king, but not fully completed

--- Chess/Pieces/King.py
+++ Chess/Pieces/King.py
@@ -1,6 +1,23 @@
+import warnings
+
 from .Piece import Piece
+from .utils import algebraic_to_numeric
 
 
 class King(Piece):
     def __init__(self, color):
         Piece.__init__(self, 'King', color)
+
+    def is_valid_move(self, start_pos, end_pos, board):
+        warnings.warn("King needs to check that in the new position the king is not in check")
+
+        start_pos_numeric = algebraic_to_numeric(start_pos)
+        end_pos_numeric = algebraic_to_numeric(end_pos)
+
+        board_at_end_pos = board[end_pos_numeric[0], end_pos_numeric[1]]
+
+        if abs(end_pos_numeric[0] - start_pos_numeric[0]) <= 1\
+            and abs(end_pos_numeric[0] - start_pos_numeric[0]) <= 1:
+            return board_at_end_pos.color != self.color
+
+        return False
