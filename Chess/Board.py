1c63991d0465fabc15607664cf912836202999e4
infamoussoap <james.chok1000+github@gmail.com>
2022-07-23 19:43:05 +1000

    changing the numeric coordinates so it matches algebraic coordinates

--- Chess/Board.py
+++ Chess/Board.py
@@ -7,6 +7,7 @@ from .DefaultBoard import default_board
 class Board:
     def __init__(self):
         self.board = np.array([[Pieces.Blank() for col in range(8)] for row in range(8)])
+        self.move_count = 0
 
     def reset(self):
         self.board = default_board()
@@ -17,8 +18,16 @@ class Board:
         for row in reversed(range(8)):
             print(blank_row)
 
-            pieces_in_row = "".join([f"| {piece.icon} " for piece in self.board[row]])
+            pieces_in_row = "".join([f"| {piece.icon} " for piece in self.board[:, row]])
             print(str(row + 1) + pieces_in_row + "|")
 
         print(blank_row)
         print('   a   b   c   d   e   f   g   h ')
+
+    def move(self):
+        if self.move_count % 2 == 0:  # White to move
+            pass
+        else:  # Black to move
+            pass
+
+        self.move_count += 1
--- Chess/DefaultBoard.py
+++ Chess/DefaultBoard.py
@@ -16,29 +16,29 @@ def default_board():
 
 def set_rooks(board):
     board[0, 0] = Pieces.Rook('white')
-    board[0, 7] = Pieces.Rook('white')
-    board[7, 0] = Pieces.Rook('black')
+    board[7, 0] = Pieces.Rook('white')
+    board[0, 7] = Pieces.Rook('black')
     board[7, 7] = Pieces.Rook('black')
 
 def set_knights(board):
-    board[0, 1] = Pieces.Knight('white')
-    board[0, 6] = Pieces.Knight('white')
-    board[7, 1] = Pieces.Knight('black')
-    board[7, 6] = Pieces.Knight('black')
+    board[1, 0] = Pieces.Knight('white')
+    board[6, 0] = Pieces.Knight('white')
+    board[1, 7] = Pieces.Knight('black')
+    board[6, 7] = Pieces.Knight('black')
 
 def set_bishops(board):
-    board[0, 2] = Pieces.Bishop('white')
-    board[0, 5] = Pieces.Bishop('white')
-    board[7, 2] = Pieces.Bishop('black')
-    board[7, 5] = Pieces.Bishop('black')
+    board[2, 0] = Pieces.Bishop('white')
+    board[5, 0] = Pieces.Bishop('white')
+    board[2, 7] = Pieces.Bishop('black')
+    board[5, 7] = Pieces.Bishop('black')
 
 def set_queen_and_king(board):
-    board[0, 3] = Pieces.Queen('white')
-    board[0, 4] = Pieces.King('white')
-    board[7, 3] = Pieces.Queen('black')
-    board[7, 4] = Pieces.King('black')
+    board[3, 0] = Pieces.Queen('white')
+    board[4, 0] = Pieces.King('white')
+    board[3, 7] = Pieces.Queen('black')
+    board[4, 7] = Pieces.King('black')
 
 def set_pawns(board):
     for i in range(8):
-        board[1, i] = Pieces.Pawn('white')
-        board[6, i] = Pieces.Pawn('black')
+        board[i, 1] = Pieces.Pawn('white')
+        board[i, 6] = Pieces.Pawn('black')
